85e4f11a2623b0553cf5a5e0a47ac8c734b30996
day2 <latha.krishnayya@gmail.com>
2021-10-31 19:35:28 +0100

    Todo IT final commit

---
+++ src/main/java/se/lexicon/sneha/modal/Person.java
@@ -0,0 +1,68 @@
+package se.lexicon.sneha.model;
+
+import java.util.UUID;
+
+public class Person {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+    public Person(int id, String firstName, String lastName, String email) {
+        if (firstName == null) {
+            throw new RuntimeException("Firstname cannot be null");
+        } else if (lastName == null) {
+            throw new RuntimeException("lastname cannot be null");
+        } else if (email == null) {
+            throw new RuntimeException("email cannot be null");
+        } else {
+
+            setId(id);
+            setFirstName(firstName);
+            setLastName(lastName);
+            setEmail(email);
+
+        }
+    }
+
+    public Person(String  firstName, String lastName, String email) {
+        this(UUID.randomUUID().variant(), firstName,lastName, email);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSummary(){
+        return "{" + " ID : "+id+ ", Name : "+firstName+" "+lastName+", Email : "+email+" }";
+    }
+}
---
+++ src/main/java/se/lexicon/sneha/modal/TodoItem.java
@@ -0,0 +1,101 @@
+package se.lexicon.sneha.model;
+
+import java.time.LocalDate;
+
+
+public class TodoItem {
+    private int id;
+    String title;
+    String taskDescription;
+    LocalDate deadLine;
+    boolean done;
+    Person creator;
+
+    public TodoItem(int id, String title, String taskDescription, LocalDate deadLine, boolean done, Person creator) {
+        if( title == null){
+            throw new RuntimeException(" Task cannot be null ");
+        } else if( deadLine == null){
+            throw new RuntimeException(" must specify the Deadline for the task");
+        }else {
+            this.id = id;
+            this.title = title;
+            this.taskDescription = taskDescription;
+            this.deadLine = deadLine;
+            this.done = done;
+            this.creator = creator;
+        }
+    }
+
+    public TodoItem(int id,
+                    String title,
+                    String taskDescription,
+                    String targetDate, boolean done,
+                    Person creator) {
+
+        this(id,title,taskDescription,LocalDate.parse(targetDate), done, creator);
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public LocalDate getDeadLine() {
+        return deadLine;
+    }
+
+    public void setDeadLine(LocalDate deadLine) {
+        this.deadLine = deadLine;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Person getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Person creator) {
+
+        this.creator = creator;
+    }
+    public boolean isOverdue(){
+        LocalDate currentDate= LocalDate.now();
+        if(isDone()){
+            System.out.println("TASK IS ALREADY FINISHED");
+            return false;
+        }else
+        return currentDate.isAfter(deadLine);
+
+    }
+    public String getSummary(){
+        return "{ " +"Name : "+creator.getFirstName()+" "+creator.getLastName()+ ", " +
+                "Title : "+getTitle()+", Task Description : "+getTaskDescription()+" }";
+    }
+
+}
